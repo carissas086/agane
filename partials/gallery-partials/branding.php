1ce68b42f6d231f0948887eb00903dd8890cf377
EunhyeJang <carissaso@gmail.com>
2022-07-17 19:17:51 -0700

    links no .. or /

--- partials/gallery-partials/branding.php
+++ partials/gallery-partials/branding.php
@@ -2,11 +2,11 @@
     <h3>Corporate Branding</h3>
     <p class="gal-sub">Logo and Business Asset Design Projects</p>
     <div class="gal flex flex-vertcen row">
-        <a class="gal-link col-12 col-md-6" href="/design/compository.php">
+        <a class="gal-link col-12 col-md-6" href="design/compository.php">
             <p class="head-font gal-hov">The Compository</p>
             <div class="gal-img" id="gal-cmp"></div>
         </a>
-        <a class="gal-link col-12 col-md-6" href="/design/pacificmeta.php">
+        <a class="gal-link col-12 col-md-6" href="design/pacificmeta.php">
             <p class="head-font gal-hov">Pacific Meta</p>
             <div class="gal-img" id="gal-pmsg"></div>
         </a>
