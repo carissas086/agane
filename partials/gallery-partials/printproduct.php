8d0c04d08494ff79569b30026e0acdc1d19d95c6
EunhyeJang <carissaso@gmail.com>
2022-07-17 19:25:12 -0700

    design partials index.php link edit
    no .. or /

--- partials/gallery-partials/printproduct.php
+++ partials/gallery-partials/printproduct.php
@@ -2,21 +2,21 @@
     <h3>Print Layout &amp; Packaging</h3>
     <p class="gal-sub">Product Packaging and Print Poster Projects</p>
     <div class="gal flex flex-vertcen row">
-        <a class="gal-link col-12 col-md-6" href="/design/littleredteahouse.php#lrth-1">
+        <a class="gal-link col-12 col-md-6" href="design/littleredteahouse.php#lrth-1">
             <p class="head-font gal-hov">Little Red TeaHouse</p>
             <div class="gal-img" id="gal-lrth"></div>
         </a>
-        <a class="gal-link col-12 col-md-6" href="/design/noclowns.php#nccr-1">
+        <a class="gal-link col-12 col-md-6" href="design/noclowns.php#nccr-1">
             <p class="head-font gal-hov">No Clowns Coffee Roasters</p>
             <div class="gal-img" id="gal-nccr"></div>
         </a>
     </div>
     <div class="gal flex flex-vertcen row">
-        <a class="gal-link col-12 col-md-6" href="/design/sneakz.php#snkz-1">
+        <a class="gal-link col-12 col-md-6" href="design/sneakz.php#snkz-1">
             <p class="head-font gal-hov">SNEAKZ</p>
             <div class="gal-img" id="gal-snkz"></div>
         </a>
-        <a class="gal-link col-12 col-md-6" href="/design/chess.php#fvc-1">
+        <a class="gal-link col-12 col-md-6" href="design/chess.php#fvc-1">
             <p class="head-font gal-hov">Fraser Valley Championship</p>
             <div class="gal-img" id="gal-fvc"></div>
         </a>
--- partials/gallery-partials/webmarketing.php
+++ partials/gallery-partials/webmarketing.php
@@ -2,7 +2,7 @@
     <h3>Web Marketing Assets</h3>
     <p class="gal-sub">Web Advertising Asset Layout Projects</p>
     <div class="gal flex flex-vertcen row">
-        <a class="gal-link col-12 col-md-6" href="/design/marketingbanners.php#mb-1">
+        <a class="gal-link col-12 col-md-6" href="design/marketingbanners.php#mb-1">
             <p class="head-font gal-hov">Origins Parkour Gym Marketing Banners</p>
             <div class="gal-img" id="gal-origins"></div>
         </a>
@@ -12,33 +12,33 @@
         </a>
     </div>
     <div class="gal flex flex-vertcen row">
-        <a class="gal-link col-12 col-md-3" href="/design/blackmansion-posts.php#bm-1">
+        <a class="gal-link col-12 col-md-3" href="design/blackmansion-posts.php#bm-1">
             <p class="head-font gal-hov">Black Mansion Marketing Post &mdash; Library</p>
             <div class="gal-img" id="gal-bm-1"></div>
         </a>
-        <a class="gal-link col-12 col-md-3" href="/design/blackmansion-posts.php#bm-2">
+        <a class="gal-link col-12 col-md-3" href="design/blackmansion-posts.php#bm-2">
             <p class="head-font gal-hov">Black Mansion Marketing Posts &mdash; Hall</p>
             <div class="gal-img" id="gal-bm-2"></div>
         </a>
-        <a class="gal-link col-12 col-md-3" href="/design/blackmansion-posts.php#bm-3">
+        <a class="gal-link col-12 col-md-3" href="design/blackmansion-posts.php#bm-3">
             <p class="head-font gal-hov">Black Mansion Marketing Posts &mdash; Field</p>
             <div class="gal-img" id="gal-bm-3"></div>
         </a>
-        <a class="gal-link col-12 col-md-3" href="/design/letterstocody.php#ltc-1">
+        <a class="gal-link col-12 col-md-3" href="design/letterstocody.php#ltc-1">
             <p class="head-font gal-hov">Letters To Cody &mdash; Instagram Post</p>
             <div class="gal-img" id="gal-ltc"></div>
         </a>
     </div>
     <div class="gal flex flex-vertcen row">
-        <a class="gal-link col-12 col-md-4" href="/design/youthepower.php#ytp-1">
+        <a class="gal-link col-12 col-md-4" href="design/youthepower.php#ytp-1">
             <p class="head-font gal-hov">Youthe Power Halloween Posts</p>
             <div class="gal-img" id="gal-ytp-1"></div>
         </a>
-        <a class="gal-link col-12 col-md-4" href="/design/youthepower.php#ytp-2">
+        <a class="gal-link col-12 col-md-4" href="design/youthepower.php#ytp-2">
             <p class="head-font gal-hov">Youthe Power Instagram Campaign Post</p>
             <div class="gal-img" id="gal-ytp-2"></div>
         </a>
-        <a class="gal-link col-12 col-md-4" href="/design/youthepower.php#ytp-3">
+        <a class="gal-link col-12 col-md-4" href="design/youthepower.php#ytp-3">
             <p class="head-font gal-hov">Youthe Power Facebook Events Graphics</p>
             <div class="gal-img" id="gal-ytp-3"></div>
         </a>
