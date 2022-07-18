a804e74ca5c59993ace3d056c7169af500231d28
EunhyeJang <carissaso@gmail.com>
2022-07-17 19:26:39 -0700

    lightbox script link edit
    one wonders at what point this is a good idea...

--- index.php
+++ index.php
@@ -86,7 +86,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" ></script>
 
     <!-- lightbox -->
-        <script src="../scripts/content-plugins.js"></script>
+        <script src="scripts/content-plugins.js"></script>
 
     <!-- bootstrap -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
