2c54b01744dff8e00987e5ef1c8cbb6f307b28d6
EunhyeJang <carissaso@gmail.com>
2022-07-17 19:25:46 -0700

    index.php lightbox link
    also remove ../

--- partials/headlinks.php
+++ partials/headlinks.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="https://use.typekit.net/iho2ohe.css">
     
-<link href="../styles/lightbox.min.css" rel="stylesheet" />
+<link href="styles/lightbox.min.css" rel="stylesheet" />
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 <link rel="stylesheet" href="styles/reset.css" class="style-reset">
