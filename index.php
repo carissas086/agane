3cee6a96f499ac3c814466d955918dea0316e802
EunhyeJang <carissaso@gmail.com>
2022-08-18 05:34:43 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>carissas &mdash; creativity through design</title>
-    
-<!--    last updated: 220810    -->
 
     <?php include __DIR__ . '/partials/headlinks.php'; ?>
     
