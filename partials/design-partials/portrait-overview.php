1ca524904b70e93a770f77342415f77f7de78454
EunhyeJang <carissaso@gmail.com>
2022-12-04 01:22:25 -0800

    'Portrait' artifact up

---
+++ partials/design-partials/portrait-overview.php
@@ -0,0 +1,18 @@
+<h2 class="head-font arti-head">Project Overview</h2>
+<div class="content-arti-clean flex flex-around flex-end">
+    <div class="content-arti-final framed">
+        <a href="../images/cs_portrait-sketch.jpg"
+            data-lightbox="portrait"
+            data-title="Initial sketch done in Photoshop of the composite photo I wanted to make. I chose to sketch in white on black in order to indicate to the subject, and as a guide to how I wanted to shoot the photos, how light would work in the piece." >
+                <img src="../images/cs_portrait-sketch.jpg" alt="black and white sketch of composite photograph">
+        </a>
+    </div>
+    <div class="content-arti-copy">
+        <p>The concept for this project began with a curious statement often made by <a href="https://roamresearch.com/#/app/JosephSo___Library/page/HsewQ0Z5j" target="_blank" title="Joseph's Public Library" class="art-link">Joseph So</a>. As a composer and music producer, Joseph (sometimes adopting the handle 'The Spaceman') would say that he's a musician, whose instrument is 'the computer'. The goal with this project was, therefore, to create a piece that would show what that looks like, to make '<span class="bold">a portrait of the Spaceman</span>'.</p>
+        <h3 class="bold">Tools Utilised</h3>
+        <ul class="li-indent bullets">
+            <li>Fujifilm XT-3</li>
+            <li>Adobe Photoshop</li>
+        </ul>
+    </div>
+</div>
