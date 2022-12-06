75cb8cc3c66baa2d1af5b823d8bd696e0f18ed0e
EunhyeJang <carissaso@gmail.com>
2022-12-06 00:42:06 -0800

    tc link final video

--- partials/design-partials/bcittc-mogr.php
+++ partials/design-partials/bcittc-mogr.php
@@ -6,6 +6,7 @@
             <li>3D animated graphics <span class="italics">(done entirely in Blender)</span></li>
             <li>purely text-based motion graphics <span class="italics">(done in Adobe After Effects)</span></li>
         </ul>
+        <p>All assets created were handed off to be edited into the <a href="https://www.youtube.com/watch?v=zGL-l4sZIJ4" target="_blank" title="Final promotional video created" class="art-link">final version of the video</a> and uploaded online.</p>
     </div>
 </div>
 <div class="content-arti-clean flex flex-around flex-end flip video-box">
